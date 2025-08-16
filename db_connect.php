bbcf16dc6626f015a4ca1004dfa49551b81d9e8e
Ayush7089020 <myfreefirre@gmail.com>
2025-08-16 11:12:47 +0530

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,9 @@
 require_once 'config.php';
 
 try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $pdo = new PDO("mysql:host=sql300.epizy.com;dbname=" . DB_NAME, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
-
 ?>
